98fc5eac1eecd092eeb5c246707f4289e7bdffcd
Eduardo Rodrigues <mrodrigues.eduardo@gmail.com>
2017-02-10 19:34:02 -0200

    Cria funcionalidade do crud e pesquisa

---
+++ src/main/java/com/eduardo/vinhos/repository/VinhoFilter.java
@@ -0,0 +1,14 @@
+package com.eduardo.vinhos.repository;
+
+public class VinhoFilter {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
--- src/main/java/com/eduardo/vinhos/repository/Vinhos.java
+++ src/main/java/com/eduardo/vinhos/repository/Vinhos.java
@@ -1,9 +1,12 @@
 package com.eduardo.vinhos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eduardo.vinhos.model.Vinho;
 
 public interface Vinhos extends JpaRepository<Vinho, Long>{
 	
+	List<Vinho> findByNomeContainingIgnoreCase(String nome);
 }
